2fc545da445b21de6008906f4b549aa9f64a98f3
Raskollnikov <bakhalaia56@gmail.com>
2025-05-11 16:53:04 +0400

    protecter routes, password routes added, UI fixed

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,24 +1,27 @@
 import {motion} from 'framer-motion'
-import { Link, Navigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Input from '../components/Input';
-import {MailIcon, User,Lock} from 'lucide-react'
+import {MailIcon, User,Lock,Loader} from 'lucide-react'
 import { useState } from 'react';
 import PasswordStrengthMeter from '../components/PasswordStrengthMeter';
+import { useAuthStore } from '../store/authStore';
 
 const SignUpPage = () => {
   const [name,setName] = useState('')
   const [email,setEmail] = useState('')
   const [password,setPassword] = useState('')
 
+  const {signup,error,isLoading} = useAuthStore();
+const navigate = useNavigate();  
   const handleSignUp = async (e) => {
 		e.preventDefault();
 
-		// try {
-		// 	await signup(email, password, name);
-		// 	Navigate("/verify-email");
-		// } catch (error) {
-		// 	console.log(error);
-		// }
+		try {
+			await signup(email, password, name);       
+			navigate("/verify-email");
+		} catch (error) {
+			console.log(error);
+		}
 	};
 
   return (
@@ -38,7 +41,7 @@ const SignUpPage = () => {
             <Input icon={User} type="text" placeholder="Full Name" value={name} onChange={e=>setName(e.target.value)}/>
             <Input icon={MailIcon} type="email" placeholder="Email Address" value={email} onChange={e=>setEmail(e.target.value)}/>
             <Input icon={Lock} type="password" placeholder="password" value={password} onChange={e=>setPassword(e.target.value)}/>
-
+            {error && <p className='text-red-500 font-semibold mt-2'>{error}</p>}
             <PasswordStrengthMeter password={password} />
 
             <motion.button
@@ -49,10 +52,10 @@ const SignUpPage = () => {
 						whileHover={{ scale: 1.02 }}
 						whileTap={{ scale: 0.98 }}
 						type='submit'
-						// disabled={isLoading}
+						disabled={isLoading}
 					>
             Sign Up
-						{/* {isLoading ? <Loader className=' animate-spin mx-auto' size={24} /> : "Sign Up"} */}
+						{isLoading ? <Loader className=' animate-spin mx-auto' size={24} /> : "Sign Up"}
 					</motion.button>
         </form>
       </div>
