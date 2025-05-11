c48765a46d7b132b5589a7bf1d5af511deed155a
Raskollnikov <bakhalaia56@gmail.com>
2025-05-11 17:05:15 +0400

    deployment

--- backend/index.js
+++ backend/index.js
@@ -3,15 +3,31 @@ import dotenv from 'dotenv'
 import { connectDB } from './db/connectDb.js';
 import authRoutes from './routes/auth.route.js'
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import path from 'path'
 
 const app=express();
+
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+
 app.use(express.json());
 app.use(cookieParser())
 const PORT=process.env.PORT || 5000
 
 dotenv.config();
 
+const __dirname=path.resolve();
+
 app.use('/api/auth',authRoutes)
+
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 app.listen(PORT,()=>{
     connectDB();
